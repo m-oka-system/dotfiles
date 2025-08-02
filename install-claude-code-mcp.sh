1b149e1c689f82457171d7b2d34cc0c7068147e3
m-oka-system <m-oka-system@outlook.com>
2025-08-02 10:24:43 +0900

    feat: GitHubのMCPサーバーを追加

--- install-claude-code-mcp.sh
+++ install-claude-code-mcp.sh
@@ -11,3 +11,6 @@ claude mcp add -s project Playwright npx @playwright/mcp@latest
 
 # Context7
 claude mcp add -s project Context7 -- npx -y @upstash/context7-mcp@latest
+
+# GitHub
+claude mcp add -s project Github -- docker run -i --rm -e GITHUB_PERSONAL_ACCESS_TOKEN ghcr.io/github/github-mcp-server
