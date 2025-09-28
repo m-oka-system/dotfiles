51efe1fcecdbbccba304bdde8270d180c3305b24
m-oka-system <m-oka-system@outlook.com>
2025-09-28 21:36:31 +0900

    feat: Chrome DevToolsのインストールを追加

--- install-claude-code-mcp.sh
+++ install-claude-code-mcp.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 # Azure MCP Server
-claude mcp add -s project Azure npx @azure/mcp@latest server start
+claude mcp add -s user Azure npx @azure/mcp@latest server start
 
 # Terraform
-claude mcp add -s project Terraform -- docker run -i --rm hashicorp/terraform-mcp-server
+claude mcp add -s user Terraform -- docker run -i --rm hashicorp/terraform-mcp-server
 
 # Playwright
-claude mcp add -s project Playwright npx @playwright/mcp@latest
+claude mcp add -s user Playwright npx @playwright/mcp@latest
 
 # Context7
-claude mcp add -s project Context7 -- npx -y @upstash/context7-mcp@latest
+claude mcp add -s user Context7 -- npx -y @upstash/context7-mcp@latest
 
 # GitHub
-claude mcp add -s project Github -- docker run -i --rm -e GITHUB_PERSONAL_ACCESS_TOKEN ghcr.io/github/github-mcp-server
+claude mcp add -s user Github -- docker run -i --rm -e GITHUB_PERSONAL_ACCESS_TOKEN ghcr.io/github/github-mcp-server
 
 # DeepWiki
 claude mcp add -s user --transport http Deepwiki https://mcp.deepwiki.com/mcp
 
 # Serena
 claude mcp add -s local Serena -- uvx --from git+https://github.com/oraios/serena serena start-mcp-server --enable-web-dashboard False --context ide-assistant --project $(pwd)
+
+# Chrome DevTools
+claude mcp add -s user Chrome-DevTools npx chrome-devtools-mcp@latest
